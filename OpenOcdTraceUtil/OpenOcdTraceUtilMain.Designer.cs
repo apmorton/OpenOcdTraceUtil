b3ef24d3995102111724c1003fd81f0e08112df0
Austin Morton <amorton@juvsoft.com>
2015-03-07 18:19:39 -0500

    Add OpenOcdTraceUtil project

---
+++ OpenOcdTraceUtil/OpenOcdTraceUtilMain.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace OpenOcdTraceUtil
+{
+    partial class OpenOcdTraceUtilMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelConnected = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 55);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(765, 355);
+            this.textBox1.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelConnected});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 413);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(789, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelConnected
+            // 
+            this.toolStripStatusLabelConnected.Name = "toolStripStatusLabelConnected";
+            this.toolStripStatusLabelConnected.Size = new System.Drawing.Size(79, 17);
+            this.toolStripStatusLabelConnected.Text = "Disconnected";
+            // 
+            // OpenOcdTraceUtilMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 435);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "OpenOcdTraceUtilMain";
+            this.Text = "OpenOCD Trace Utility";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelConnected;
+    }
+}
+
---
+++ OpenOcdTraceUtil/OpenOcdTraceUtilMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenOcdTraceUtil
+{
+    public partial class OpenOcdTraceUtilMain : Form
+    {
+        private OpenOcdTclClient.OpenOcdTclClient client;
+
+        public OpenOcdTraceUtilMain()
+        {
+            InitializeComponent();
+            client = new OpenOcdTclClient.OpenOcdTclClient(this);
+            client.ConnectionChanged += ClientOnConnectionChanged;
+            client.TargetTrace += ClientOnTargetTrace;
+        }
+
+        private void ClientOnTargetTrace(object sender, OpenOcdTclClient.OpenOcdTclClient.TargetTraceArgs args)
+        {
+        }
+
+        private void ClientOnConnectionChanged(object sender, EventArgs e)
+        {
+            if (client.Connected)
+                toolStripStatusLabelConnected.Text = "Connected";
+            else
+                toolStripStatusLabelConnected.Text = "Disconnected";
+        }
+    }
+}
